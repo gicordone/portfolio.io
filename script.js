07c3c134cf18136c4029fb9ef1a2883a346be3ec
Giselle Cordone <cordonegiselle@gmail.com>
2022-07-24 15:56:39 -0300

    repositorio botão

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 
     var formulario = document.getElementsByName('formulario')[0]
 
+
+    var repositorios = document.getElementById('repositorios')
+    // console.log(repositorios)
+
     var validarCampos = function (e) {
         if (formulario.nome.value == 0) {
             alert("Preencher o nome");
@@ -34,11 +38,18 @@
 
 
     let data;
-    var showData;
     
     fetch(`https://api.github.com/users/gicordone/repos`)
         .then(response =>{ response.json()
-        .then(repo => {data = repo; console.log(data)})
+        .then(repo => {
+            data = repo;
+            console.log(data[0])
+            const text = 'conteudo' 
+            repositorios.innerHTML =`<div class="col">
+            <a href="${data[0].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[0].name}</button></a>
+        </div>`
+        })
+
     })
 
 }())
