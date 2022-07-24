2b4ed43b85bbc4360a5daa93fa86b8e958ca6f47
Giselle Cordone <cordonegiselle@gmail.com>
2022-07-24 16:44:00 -0300

    repositorios botões

--- script.js
+++ script.js
@@ -43,11 +43,15 @@
         .then(response =>{ response.json()
         .then(repo => {
             data = repo;
-            console.log(data[0])
+            console.log(data)
             const text = 'conteudo' 
             repositorios.innerHTML =`<div class="col">
             <a href="${data[0].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[0].name}</button></a>
-        </div>`
+        </div><div class="col">
+        <a href="${data[3].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[3].name}</button></a>
+    </div><div class="col">
+    <a href="${data[4].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[4].name}</button></a>
+</div>`
         })
 
     })
