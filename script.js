a986c460d5d6e82d275f673c9b73c52c04037fb2
Giselle Cordone <cordonegiselle@gmail.com>
2022-07-25 16:39:41 -0300

    atualização

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 
     var formulario = document.getElementsByName('formulario')[0]
 
-
     var repositorios = document.getElementById('repositorios')
-    // console.log(repositorios)
 
     var validarCampos = function (e) {
         if (formulario.nome.value == 0) {
@@ -31,32 +29,25 @@
     var validar = function (e) {
         validarCampos(e);
 
-        console.log(e)
     };
 
     formulario.addEventListener("submit", validar)
 
 
-    let data;
-    
     fetch(`https://api.github.com/users/gicordone/repos`)
-        .then(response =>{ response.json()
-        .then(repo => {
-            data = repo;
-            console.log(data)
-            const text = 'conteudo' 
-            repositorios.innerHTML =`<div class="col">
+        .then(response => {
+            response.json()
+            .then(repo => {
+
+                repositorios.innerHTML = `<div class="col">
             <a href="${data[0].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[0].name}</button></a>
         </div><div class="col">
         <a href="${data[3].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[3].name}</button></a>
     </div><div class="col">
     <a href="${data[4].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[4].name}</button></a>
 </div>`
+            })
+
         })
 
-    })
-
 }())
-
-
-// "https://api.github.com/repos/{owner}/{repo}"
