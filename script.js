8dcffc2ea7be3a46d4e07803f37d556b3718c007
Giselle Cordone <cordonegiselle@gmail.com>
2022-07-27 10:18:47 -0300

    fix

--- script.js
+++ script.js
@@ -37,18 +37,19 @@
     fetch(`https://api.github.com/users/gicordone/repos`)
         .then(response => {
             response.json()
-            .then(repo => {
-                repo = data
-
-                repositorios.innerHTML = `<div class="col">
-            <a href="${data[0].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[0].name}</button></a>
+                .then(data => {
+                    repositorios.innerHTML = `<div class="col">
+                <a href="${data[0].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[0].name}</button></a>
+            </div><div class="col">
+            <a href="${data[3].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[3].name}</button></a>
         </div><div class="col">
-        <a href="${data[3].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[3].name}</button></a>
-    </div><div class="col">
-    <a href="${data[4].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[4].name}</button></a>
-</div>`
-            })
+        <a href="${data[4].clone_url}" target="_blank"><button type="button" class="btn btn-secondary btn-lg">${data[4].name}</button></a>
+    </div>`
+                })
+
+                .catch(error => { console.log(error) })
 
         })
+
 
 }())
